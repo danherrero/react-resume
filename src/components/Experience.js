6b93b17496f163b5219a922f83b67484ae6077aa
Dan Herrero <dah314@lehigh.edu>
2021-07-05 16:47:35 -0400

    iterate over titles and responsibilities arrays

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,8 +8,23 @@ render() {
   	<div>
   		<h2>Work Experience</h2>
   		{experience.map(item => (
-  			<div><h3>{item.name}</h3>
-  			<p className="location">{item.location}</p></div>
+  			<div>
+  				<h3>{item.name}</h3>
+  				<p className="location">{item.location}</p>
+  			
+  				{item.titles.map(title => (
+      				<div>
+        	    		<h3>{title[0]} | {title[1]}</h3>
+      		  		</div>
+    			))}
+
+    			{item.responsibilities.map(responsibility => (
+      				<ul>
+        	    		<li>{responsibility}</li>
+      		  		</ul>
+    			))}
+
+    		</div>
   		))}
     </div>
   )
