498cad1ae7dcdd11555b50b089f9bc2537924393
Dan Herrero <dah314@lehigh.edu>
2021-07-05 15:31:42 -0400

    added components for About, Experience and Education

---
+++ src/components/education.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export default class Education extends Component {
+
+render() {
+  return (
+     <div className="education">
+     	<h2>Education</h2>
+     	<h3>Bachelor of Science in Media Communications &amp; Technology</h3>
+     	<p>East Stroudsburg University, PA<br />2005-2009</p>
+     </div>
+  )
+ }
+}
---
+++ src/components/experience.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+
+export default class Experience extends Component {
+
+render() {
+  return (
+     <div className="experience">
+     	<h2>Work Experience</h2>
+     	<h3>Lehigh University in Bethlehem, PA</h3>
+     	<p><strong>Title</strong>: Senior Web Development Specialist (April 2019 – Present)<br />
+     	Web Development Specialist (May 2014 – April 2019)</p>
+     	<ul>
+     		<li>Responsible for the day-to-day management and development of a Drupal-based content management system</li>
+     		<li>Collaborates with design and marketing teams to implement new web pages and updates to existing web pages on the university website</li>
+     		<li>Manually tests web pages for web accessibility issues and remediates any issues found</li>
+     		<li>Utilizes Google Analytics to analyze and report on the utilization of web pages</li>
+     		<li>Performs trend research and competitive benchmarking studies</li>
+     		<li>Provides training, account administration, and technical assistance for the content management system</li>
+     		<li>Creates project schedules, goal documents, wireframes, and other documents as part of UX/UI planning</li>
+     	</ul>
+
+     	<h3>Diocese of Allentown in Allentown, PA</h3>
+     	<p><strong>Title</strong>: Web Developer (September 2011 – May 2014)</p>
+     	<ul>
+     		<li>Developed and managed a website built upon a Concrete5-based content management system</li>
+     		<li>Collaborated with leaders in Stewardship & Development to develop an online giving platform</li>
+     		<li>Provided training and technical assistance for users of the content management system</li>
+     		<li>Managed social media accounts for the organization</li>
+     	</ul>
+
+     	<h3>Freelancer in Allentown, PA</h3>
+     	<p><strong>Title</strong>: Web Developer (January 2009 – September 2011)</p>
+     	<ul>
+     		<li>Utilized HTML, CSS, Javascript, and PHP to develop websites for clients</li>
+     		<li>Built and supported custom Wordpress themes and plugins</li>
+     		<li>Provided technical support via email and phone for clients</li>
+     	</ul>
+     </div>
+  )
+ }
+}
