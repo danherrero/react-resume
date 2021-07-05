b467de662337c7b48ef7993c78d28797e02ceeaf
Dan Herrero <dah314@lehigh.edu>
2021-07-05 15:37:08 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header'
 import About from './components/about'
 import Experience from './components/experience'
 import Education from './components/education'
@@ -7,6 +8,7 @@ import Education from './components/education'
 function App() {
   return (
     <div className="Resume">
+      <Header />
       <About />
       <Experience />
       <Education />
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,6 +6,7 @@ export default class About extends Component {
 render() {
   return (
      <div className="about">
+     	<h2>About Me</h2>
      	<p>My name is Dan Herrero.</p>
      </div>
   )
