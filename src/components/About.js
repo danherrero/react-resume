0f9e75e3ab65ea7e56edbf6a8ad9fb06818a74d8
Dan Herrero <danherrero@electriccitizen.com>
2021-11-22 20:55:42 -0500

    updated about and experience

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ return (
 <section className="about">
         <h2><span>About Me</span></h2>
         <div>
-        <p>My name is Dan Herrero, and I am a web developer living in the Lehigh Valley of Pennsylvania. For the past seven years, I have been specializing in Drupal development at Lehigh University.</p>
+        <p>My name is Dan Herrero, and I am a web developer living in the Lehigh Valley of Pennsylvania.</p>
         <img src="/images/dan.jpeg" alt="Dan smiling" className="img-fluid circle-image" />
         </div>
      </section>
